bd07fe498b6046d99ddcbb6ad265cab950d17928
Tauseef Khan <tauseef.khan@coxautoinc.com>
2022-06-18 01:31:30 -0400

    First commit

---
+++ src/main/java/com/spring/reditclone/ReditcloneApplication.java
@@ -0,0 +1,13 @@
+package com.spring.reditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReditcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReditcloneApplication.class, args);
+	}
+
+}
